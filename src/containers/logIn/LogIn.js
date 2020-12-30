db22d49783ad32a909eacee0e790086d82fb349e
Choharim <jhl8586@naver.com>
2020-12-30 17:06:38 +0900

    Add logInPage

--- src/containers/logIn/LogIn.js
+++ src/containers/logIn/LogIn.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { FaDog } from "react-icons/fa";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
   const [currentUser, setCurrentUser] = useState({ id: "", pw: "" });
   const [logInSuccess, setLogInSuccess] = useState(false);
+  const [pwError, setPwError] = useState(false);
   const str_LS_users = localStorage.getItem("users");
   const pars_LS_users = JSON.parse(str_LS_users);
   const { id, pw } = currentUser;
@@ -14,14 +17,22 @@ const LogIn = () => {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (currentUser.pw.length < 3 || currentUser.pw.length > 10) {
+      setPwError(true);
+    } else {
+      setPwError(false);
+    }
     if (pars_LS_users !== null) {
       pars_LS_users.forEach((user) => {
         if (user.id === currentUser.id && user.pw === currentUser.pw) {
           setLogInSuccess(true);
         }
       });
+    } else {
+      alert("Please Sign Up");
     }
   };
+
   if (logInSuccess) {
     localStorage.setItem("currentUser", JSON.stringify(currentUser));
   }
@@ -35,8 +46,13 @@ const LogIn = () => {
         <IDInput onChange={handleChange("id")} />
         <PWLabel>Password</PWLabel>
         <PWInput onChange={handleChange("pw")} />
+        {pwError && <PWError>more than 3 and less than 10</PWError>}
         <SubmitBtn type="submit">Log In</SubmitBtn>
       </LogInForm>
+      <SignUpBtn to="/signUp">
+        Sign Up
+        <SignUpIcon />
+      </SignUpBtn>
     </LogInContainer>
   );
 };
@@ -86,6 +102,14 @@ const PWInput = styled.input`
   margin-bottom: 10px;
 `;
 
+const PWError = styled.p`
+  width: 100%;
+  margin: 0;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  text-align: center;
+`;
+
 const SubmitBtn = styled.button`
   width: 90px;
   height: 30px;
@@ -93,4 +117,21 @@ const SubmitBtn = styled.button`
   border-radius: 5px;
   padding: 5px 0;
   align-self: flex-end;
+`;
+
+const SignUpBtn = styled(Link)`
+  width: 40%;
+  margin-top: 10px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  text-decoration: none;
+  &:visited {
+    color: black;
+  }
+`;
+
+const SignUpIcon = styled(AiOutlineArrowRight)`
+  font-size: 1.5rem;
+  padding-left: 5px;
 `;
