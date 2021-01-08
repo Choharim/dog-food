e763fe10a0bbcc8b9228ead4ad93994b5ea8a115
Choharim <jhl8586@naver.com>
2021-01-08 12:48:18 +0900

    Show Pucture of  currentUser

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -11,13 +11,21 @@ import { Link } from "react-router-dom";
 const Home = () => {
   const [showNavbar, setShowNavbar] = useState(false);
   const [isUser, setIsUser] = useState(false);
+  const [currentUserInfo, setCurrentUserInfo] = useState({});
   const [foodArray, setFoodArray] = useState(Data);
   const [showMenuDetails, setShowMenuDetails] = useState({});
   const [favorite, setFavorite] = useState([]);
 
   useEffect(() => {
     if (localStorage.getItem("currentUser")) {
+      const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+      const users = JSON.parse(localStorage.getItem("users"));
       setIsUser(true);
+      setCurrentUserInfo(
+        users.find(
+          (user) => user.id === currentUser.id && user.pw === currentUser.pw
+        )
+      );
     } else {
       setIsUser(false);
     }
@@ -38,11 +46,15 @@ const Home = () => {
   const filterFoodArray = (category) => {
     setFoodArray(Data.filter((food) => food.category === category));
   };
+  console.log(currentUserInfo);
 
   return (
     <>
       {Object.keys(showMenuDetails).length === 0 ? (
         <HomeContainer>
+          {isUser && currentUserInfo.userPicture !== "" && (
+            <UserPicture image={currentUserInfo.userPicture}></UserPicture>
+          )}
           <BarIcon
             showNavbar={showNavbar}
             onClick={() => setShowNavbar(!showNavbar)}
@@ -83,6 +95,17 @@ const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+const UserPicture = styled.div`
+  margin: 10px 10px 0 0;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  position: relative;
+  right: -40%;
+  background-image: url(${(props) => props.image});
+  background-size: cover;
 `;
 
 const BarIcon = styled(FaBars)`
